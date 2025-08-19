94072c604efcde02d508b69b6cff6428819c376c
DavideCapuozzo <devsoloweb@gmail.com>
2025-08-19 14:47:51 +0200

    feat: initialize server with TypeScript, Express, and MongoDB connection
    - Added package.json for server dependencies and scripts.
    - Created server.ts to set up Express server with CORS and cookie parser.
    - Established MongoDB connection using Mongoose.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ server/server.ts
@@ -0,0 +1,38 @@
+import express = require('express')
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+/* const authRouter = require('./routes/auth/auth-routes') */
+
+// create a database connection'
+mongoose.connect('mongodb+srv://devsoloweb:3bkK56PRZWH3pH8v@cluster0.vx3osfa.mongodb.net/').then(()=>console.log('MongoDB Connected')).catch((error: unknown) => console.log(error));
+//devsoloweb
+//3bkK56PRZWH3pH8v
+//mongodb+srv://devsoloweb:3bkK56PRZWH3pH8v@cluster0.vx3osfa.mongodb.net/
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            "Auttorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials : true
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+/* app.use('/api/auth', authRouter) */
+
+
+
+
+app.listen(PORT, ()=> console.log(`Server is now running on port ${PORT}`))
