c299f86fb6832fe79f75c659434d8d089147c05f
DavideCapuozzo <devsoloweb@gmail.com>
2025-09-23 21:35:38 +0200

    feat: Update NotFound page layout with Menu component and improved styling

--- client/src/pages/not-found/index.tsx
+++ client/src/pages/not-found/index.tsx
@@ -1,11 +1,21 @@
-import { LoginForm } from "@/components/auth-component/login-form"
+import { Menu } from "@/components/dashboard-component/menu"
+import { Link } from "react-router-dom"
 
 
 function NotFound() {
     return(
         <>
+            <div className="w-full h-screen flex flex-col">
+                <Menu />
+        
+                <div className="flex flex-col items-center justify-center flex-1">
+                    <h1 className="text-9xl font-bold">404</h1>
+                    <p className="text-2xl">page not found</p>
+                    <Link to="/myuser" className="hover:underline">Back to Home</Link>
+                </div>
+                
+            </div>
             
-            <h1>not found</h1>
         </>
     )
     
