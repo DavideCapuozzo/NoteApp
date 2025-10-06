9e15f0c5a7c7bd601f6edd16383f91dbf5a30dbb
DavideCapuozzo <devsoloweb@gmail.com>
2025-10-06 23:58:42 +0200

    refactor: Update CORS origin to use environment variable for better configuration

--- server/server.ts
+++ server/server.ts
@@ -29,7 +29,7 @@ app.use(session({
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders: [
             "Content-Type",
